9c7826308aefbfdf6277bec6992d8b9f72b7ab27
aaswenson <aaswenson@wisc.edu>
2016-12-07 18:42:17 -0600

    python script branch

--- matlab/air_vol_RPV.m
+++
@@ -1,7 +0,0 @@
-function volume_gas_RPV = air_vol_RPV(mass_water)
-
-% calculate the volume of air in the RPV given a mass of liquid
-
-global V_RPV rho_water
-
-volume_gas_RPV = V_RPV - (mass_water/rho_water);
